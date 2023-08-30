173bea5532a8e3bc2399842e0c18d70bfa612d40
Lukasx76 <lucas.f.gomes2002@gmail.com>
2023-08-30 13:40:04 -0300

    finally solving the second day problem counting_sort. I discovered that a minor change on how I count values makes all the difference. when I used frequency_arr[value] = arr.count(value) this gave me a time complexity of n2² and was a great problem. So, with the help of chat gpt, I learned that I should have use a normal counter to improve to o(n)

--- day2/counting_sort.py
+++ day2/counting_sort.py
@@ -3,21 +3,14 @@
 """
 
 def countingSort(arr):
-    
-    # defining the range of the array
-    numbers_to_count = [i for i in range(len(arr))]
-    
-    # defining the frequency of the elements of the array e.g [1,1,3,2,1] frequency of numbers of 0 to 3 = [0, 0, 0, 0]
-    number_frequency = "0" * len(numbers_to_count)
-    
-    # converting the string element to a list of ints
-    counted_arr = [int(i) for i in number_frequency]
-    print(len(number_frequency))
 
-    counting = 0
-    # counting the numbers
-    for i,j in enumerate(numbers_to_count):
-        counting += 1
-        counted_arr[i] = arr.count(j)
-    
-    return counted_arr
+    frequency_arr = "0" * 100
+    frequency_arr = [int(i) for i in frequency_arr]
+    arr = sorted(arr)
+
+    for value in arr:
+        frequency_arr[value] += 1
+
+    return frequency_arr
+
+print(countingSort([1,1,3,2,1]))
